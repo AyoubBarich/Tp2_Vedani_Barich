a04e55693b32f06cd64b4c3373d79e470516f358
v20035017 <camille.vedani@outlook.fr>
2023-03-02 12:48:33 +0100

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   ArrayFunc.py

--- ArrayFunc.py
+++ ArrayFunc.py
@@ -20,14 +20,14 @@ def Somme(Array):
             Sum+=i
         return Sum
     return Array
-
+#exchanges the first index and second Index in the givn array
 def iterate(Array:list,firstIndex,secondIndex):
     tmp=Array[firstIndex]
     Array[firstIndex]=Array[secondIndex]
     Array[secondIndex]=tmp
     return Array
     
-
+#returns the index of the minimum value in the given array
 def getMin(Array:list,firstIndex):
     min=firstIndex
     for i in range(firstIndex+1,len(Array)):
@@ -35,6 +35,6 @@ def getMin(Array:list,firstIndex):
         if Array[min] < Array[i]:
             min=i
     return min
-
+#returns a random value between first index and last index
 def GetRandomIndex(firstIndex,lastIndex):
     return randint(firstIndex,lastIndex)
